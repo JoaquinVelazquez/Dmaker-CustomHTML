f7ce925f6e5576ed07219da897d7cbdee42ac11f
Joaquin Velazquez <velaz314@gmail.com>
2022-12-26 16:43:41 -0300

    Modificando Favicon

--- static/js/favicon.js
+++ static/js/favicon.js
@@ -1,2 +1,2 @@
-let link = document.querySelectorAll("link")[12];
-link.href = 'https://iili.io/t0bnAF.png';
+let favicon = document.querySelector("link[rel='shortcut icon']")
+favicon.href = 'https://iili.io/t0bnAF.png';
